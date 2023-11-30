bd5017cee2752092d4a21fb27d0722353ec62990
Jsnn <jsnnmsc0421@gmail.com>
2023-11-30 11:46:33 +0800

    Update exercise.py

--- 2023-11-30 class of student/exercise.py
+++ 2023-11-30 class of student/exercise.py
@@ -4,8 +4,8 @@ class Student:
         self,
         school_address="None",
         school="None",
-        department="None",
-        manager="None",
+        major="None",
+        chair="None",
         student="None",
         ID="None",
         personal_address="None",
@@ -14,8 +14,8 @@ class Student:
     ):
         self.school_address = school_address
         self.school = school
-        self.department = department
-        self.manager = manager
+        self.major = major
+        self.chair = chair
         self.student = student
         self.ID = ID
         self.personal_address = personal_address
@@ -24,7 +24,7 @@ class Student:
 
     # print out
     def __str__(self):
-        return f"School_address: {self.school_address}\nSchool: {self.school}\nDepartment: {self.department}\nManager: {self.manager}\nStudent: {self.student}\nID: {self.ID}\nPersonal_address: {self.personal_address}\nCredit: {self.credit}\nGPA: {self.gpa}"
+        return f"School_address: {self.school_address}\nSchool: {self.school}\nMajor: {self.major}\nChair: {self.chair}\nStudent: {self.student}\nID: {self.ID}\nPersonal_address: {self.personal_address}\nCredit: {self.credit}\nGPA: {self.gpa}"
 
     # down below are get and set function
     def set_school_address(self, adr):
@@ -39,17 +39,17 @@ class Student:
     def getANDset_school(self, name):
         self.set_school(name)
 
-    def set_department(self, dpm):
-        self.department = dpm
+    def set_major(self, mjr):
+        self.major = mjr
 
-    def getANDset_deparment(self, dpm):
-        self.set_department(dpm)
+    def getANDset_major(self, mjr):
+        self.set_major(mjr)
 
-    def set_manager(self, dm):
-        self.manager = dm
+    def set_chair(self, dm):
+        self.chair = dm
 
-    def getANDset_manager(self, dm):
-        self.set_manager(dm)
+    def getANDset_chair(self, dm):
+        self.set_chair(dm)
 
     def set_student(self, name):
         self.student = name
@@ -87,23 +87,23 @@ print("-----------Before edit-----------")
 info = Student()
 print(info)
 
-print("-----------After edit-----------")
+print("-----------After edit------------")
 
 school_adr = "Tainan city yongkang district"
 school = "STUST"
-department = "CSE"
-manager = "Gwo-Jiun Horng"
+major = "CSE"
+chair = "Gwo-Jiun Horng"
 student = "Jason"
-id = "4B0G0093"
+id = "4B0G----"
 personal_adr = "Tainan city yongkang district"
 credit = "97"
-gpa = "2.8"
+gpa = "4.1"
 
 # get and set all information
 info.getANDset_school_address(school_adr)
 info.getANDset_school(school)
-info.getANDset_deparment(department)
-info.getANDset_manager(manager)
+info.getANDset_major(major)
+info.getANDset_chair(chair)
 info.getANDset_student(student)
 info.getANDset_ID(id)
 info.getANDset_personal_address(personal_adr)
