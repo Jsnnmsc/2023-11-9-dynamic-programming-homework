e29190e738d04a77f3c41075cb8fea052af28993
Jsnn <jsnnmsc0421@gmail.com>
2023-11-30 11:36:57 +0800

    edit

--- 2023-11-30 class of student/exercise.py
+++ 2023-11-30 class of student/exercise.py
@@ -1,4 +1,5 @@
 class Student:
+    # initialize
     def __init__(
         self,
         school_address="None",
@@ -21,64 +22,67 @@ class Student:
         self.credit = credit
         self.gpa = gpa
 
+    # print out
     def __str__(self):
         return f"School_address: {self.school_address}\nSchool: {self.school}\nDepartment: {self.department}\nManager: {self.manager}\nStudent: {self.student}\nID: {self.ID}\nPersonal_address: {self.personal_address}\nCredit: {self.credit}\nGPA: {self.gpa}"
 
+    # down below are get and set function
     def set_school_address(self, adr):
         self.school_address = adr
 
-    def get_school_address(self, adr):
+    def getANDset_school_address(self, adr):
         self.set_school_address(adr)
 
     def set_school(self, name):
         self.school = name
 
-    def get_school(self, name):
+    def getANDset_school(self, name):
         self.set_school(name)
 
     def set_department(self, dpm):
         self.department = dpm
 
-    def get_deparment(self, dpm):
+    def getANDset_deparment(self, dpm):
         self.set_department(dpm)
 
     def set_manager(self, dm):
         self.manager = dm
 
-    def get_manager(self, dm):
+    def getANDset_manager(self, dm):
         self.set_manager(dm)
 
     def set_student(self, name):
         self.student = name
 
-    def get_student(self, name):
+    def getANDset_student(self, name):
         self.set_student(name)
 
     def set_ID(self, id):
         self.ID = id
 
-    def get_ID(self, id):
+    def getANDset_ID(self, id):
         self.set_ID(id)
 
     def set_personal_address(self, adr):
         self.personal_address = adr
 
-    def get_personal_address(self, adr):
+    def getANDset_personal_address(self, adr):
         self.set_personal_address(adr)
 
     def set_credit(self, cdt):
         self.credit = cdt
 
-    def get_credit(self, cdt):
+    def getANDset_credit(self, cdt):
         self.set_credit(cdt)
 
     def set_GPA(self, sc):
         self.gpa = sc
 
-    def get_GPA(self, sc):
+    def getANDset_GPA(self, sc):
         self.set_GPA(sc)
 
 
+# initialize
 print("-----------Before edit-----------")
 info = Student()
 print(info)
@@ -95,14 +99,15 @@ personal_adr = "Tainan city yongkang district"
 credit = "97"
 gpa = "2.8"
 
-info.get_school_address(school_adr)
-info.get_school(school)
-info.get_deparment(department)
-info.get_manager(manager)
-info.get_student(student)
-info.get_ID(id)
-info.get_personal_address(personal_adr)
-info.get_credit(credit)
-info.get_GPA(gpa)
+# get and set all information
+info.getANDset_school_address(school_adr)
+info.getANDset_school(school)
+info.getANDset_deparment(department)
+info.getANDset_manager(manager)
+info.getANDset_student(student)
+info.getANDset_ID(id)
+info.getANDset_personal_address(personal_adr)
+info.getANDset_credit(credit)
+info.getANDset_GPA(gpa)
 
 print(info)
